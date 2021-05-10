84af098ed225a69f636b65c1a961f940d92d8ccd
Neutrino Sae B Kusrorong <neutrino.sae.b.kusrorong@geteasycash.asia>
2021-05-10 17:57:54 +0700

    Initial commit to something good

---
+++ src/main/java/com/github/kneotrino/kneotrinocrmrest/KneotrinoCrmRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.kneotrino.kneotrinocrmrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KneotrinoCrmRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KneotrinoCrmRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kneotrino/kneotrinocrmrest/KneotrinoCrmRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kneotrino.kneotrinocrmrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KneotrinoCrmRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
